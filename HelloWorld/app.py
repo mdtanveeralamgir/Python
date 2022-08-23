22ef5d095b358d95406b5b4e502501f2cc546bfc
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-23 13:21:06 -0400

    exponent, augmented assignment, precedence

---
+++ HelloWorld/app.py
@@ -0,0 +1,13 @@
+#DIFFERENT OPERAND
+print(10 / 3) #returns floting point number
+print(10 // 3) #retuns integer
+print(10 ** 2) #exponent, 10 to the power 2
+
+#AUGMENTED ASSIGNMENT
+X = 10
+X += 3 #Augmented assignment
+
+#OPERATOR PRECEDENCE
+# parenthesis > exponentiation > division > multipication > addition > substraction
+x = 10 +  2 * 10 / 2
+print(x)
