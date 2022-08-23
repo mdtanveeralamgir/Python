1be11c3a890d31d41b924f114470ed7cb234d781
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-23 12:38:03 -0400

    type casting

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,3 +1,11 @@
-name = input('What is your name? ')
-favColor = input('Whats your favorite color? ')
-print(name + '\'s favorite color is ' + favColor )
+# name = input('What is your name? ')
+# favColor = input('Whats your favorite color? ')
+# print(name + '\'s favorite color is ' + favColor )
+
+# Type Casting
+
+birth_year = input('Your birth Year? ')
+print(type(birth_year)) #Type of birth_year
+age = 2022 - int(birth_year) #casting into int
+
+print(age)
