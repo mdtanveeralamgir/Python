eace889323fdf39a7c552393aafca0624fc6c506
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-24 12:49:08 -0400

    function, argument, parameter, keyword argument, positional argument

---
+++ HelloWorld/app.py
@@ -0,0 +1,31 @@
+#FUNCTION
+"""
+
+def greet_user(name):
+    print('oi ' + name)
+
+#positional argument
+greet_user('opel')
+
+"""
+
+#KEYWORD ARGUMENT
+"""
+def name(firstName, lastName):
+    print(firstName + lastName)
+
+
+name(lastName='Alamgir', firstName='Tanveer')
+name("Tanveer", lastName='Alamgir') #Keyword argument have to come after positional argument
+
+"""
+
+# RETURN STATEMENT
+
+
+def square(number):
+    return number*number
+
+
+print(square(5))
+
