e1610a6770c2f0dcd4f6cafbe0fa7f565a6628da
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-24 10:45:44 -0400

    initialize tuples, unpacking

---
+++ HelloWorld/app.py
@@ -0,0 +1,14 @@
+#TUPLES
+
+#Tuples are immutable
+"""
+num = (1,2,3) #First brakets for tuples
+print(num.count(1))
+print(num.index(2))
+
+"""
+
+#Unpacking
+coordinates = (1,2,3)
+x, y, z = coordinates #Each item starting from index 0 - end will assign to x,y,z respectavly
+print(y) #prints 2
