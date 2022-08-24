614044a53105e0927e282fdd6326b0e814bbe2e2
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-24 00:03:46 -0400

    car game

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -6,7 +6,7 @@
 #     index += 1
 
 #SECRET NUMBER GAME
-
+"""
 secretNumber = 12
 userInput = int(input('Guess a number: '))
 inputCount = 0
@@ -24,3 +24,33 @@ while inputCount < 2:
         break
 else: #Executes only if the while loop condition returns false, if loop breaks before conditon is false then else will not execute
     print('Oops, you ran out of chance')
+"""
+
+#HELP GAME
+command = ""
+prevCommand = ""
+
+while True:
+    command = input(f"Enter your command: \n").lower()
+    if command == 'start':
+        if prevCommand == command:
+            print('The car is already on')
+        else:
+            print('The car has started!')
+    elif command == 'stop':
+        if prevCommand == command:
+            print('The car is already off')
+        else:
+            print('The car has stopped!')
+    elif command == 'help':
+        print("""
+            > Type 'start' to start the car
+            > Type 'stop' to stop the car
+        """)
+    elif command == 'quit':
+        print('bye')
+        break
+    else:
+        print('Sorry I don\'t understand your command, type help to get available commands')
+
+    prevCommand = command
