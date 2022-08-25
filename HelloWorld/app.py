c6724d487c0704bbef254aa78704bcdbe92bafc5
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-25 13:05:51 -0400

    import whole package or a method in a package

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,3 +1,6 @@
-import converts
+import converts #import all the functions inside that module
 
+#import specific method in the converter
+
+# from converts import kgToLbs
 print(converts.lbsToKg(50))
