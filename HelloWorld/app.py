387384b891e84ccb4a48f6864f97974fdc7f7caa
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-08-25 13:38:43 -0400

    import a default package, use random to generate random flot, int and select random item from list

---
+++ HelloWorld/app.py
@@ -0,0 +1,15 @@
+#Link for all module of python 3,8
+
+"""
+
+https://docs.python.org/3.8/py-modindex.html
+
+"""
+
+import random
+members = ['jonh', 'merry', 'josh']
+
+for i in range(1):
+    print(random.random()) #random flot number
+    print(random.randint(10, 20)) #random int with range
+    print(random.choice(members)) #select a random element from list
