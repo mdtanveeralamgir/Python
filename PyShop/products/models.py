fc60e223017418f2743cbc84564f488f31ad21f5
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-02 10:30:36 -0400

    adding migration and endpoints in url

--- PyShop/products/models.py
+++ PyShop/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model): #inherit Model from models
+    name = models.CharField(max_length=255)
+    price = models.FlotField()
+    stock = models.integerField()
+    image_url = models.CharField(max_length=2083)
+
--- PyShop/pyshop/urls.py
+++ PyShop/pyshop/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     # path('products/new/', include('products.urls')),
-
 ]
 
 
