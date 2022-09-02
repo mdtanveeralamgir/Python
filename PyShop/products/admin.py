55d38c320e921e3bd006ded644f048920a6fb5eb
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-02 14:26:40 -0400

    overriding admin panel to show product's list with specific attributes(columns name)

--- PyShop/products/admin.py
+++ PyShop/products/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 #importing Product from model which is in current dir
 from .models import Product
 
-
 #Adding Product table in admin panel
-admin.site.register(Product)
+# admin.site.register(Product)
+
+#customize product table in admin panel
+"""
+- class name starts with table name and concatinate with Admin
+- pass the model ModelAdmin from admin module
+
+"""
+class ProductAdmin(admin.ModelAdmin):
+    #override the arrtibutes in ModelAdmin class
+    list_display = ('name', 'price', 'stock')
+
+#Register the Product table using new class ProductAdmin 
+admin.site.register(Product, ProductAdmin)
+
+
+
